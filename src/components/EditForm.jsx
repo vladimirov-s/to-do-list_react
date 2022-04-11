562b3714fe22ab8c45693fe0bdb01f02a2c06861
Sergey Vladimirov <vladimirov.exceedteam@gmail.com>
2022-04-11 18:48:51 +0300

    addition, deleteAл

---
+++ src/components/EditForm.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+const EditForm = (checkbox, oldText, id) => {
+  // console.log(checkbox, oldText);
+  // const buttontEdit = document.createElement("button");
+  // const cancelButton = document.createElement("button");
+  // const editInput = document.createElement("input");
+  // editInput.id = `input-${id}`;
+  // const wrapForEdit = document.createElement("div");
+  // wrapForEdit.classList.add("editTask");
+  // cancelButton.innerText = "Cancel";
+  // buttontEdit.innerText = "Save";
+  // editInput.type = "text";
+  // editInput.value = oldText;
+  // editInput.title = "ESC для отмены, ENTER для ввода";
+  // editInput.placeholder = "Редактирование задачи";
+  // wrapForEdit.appendChild(editInput);
+  // wrapForEdit.appendChild(buttontEdit);
+  // wrapForEdit.appendChild(cancelButton);
+  // editInput.focus();
+  // return wrapForEdit;
+  return;
+};
+
+export default EditForm;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,12 +6,14 @@ const url = "http://localhost:8000";
 
 const Main = () => {
   const [allTasks, setTask] = useState([]);
+  const [isCheck, setCheckbox] = useState(Boolean);
   const [text, setText] = useState("");
   useEffect(() => {
     axios
       .get(`${url}/allTasks`)
       .then(function (response) {
         setTask(response.data.data);
+        console.log(response.data.data);
       })
       .catch(function (error) {
         console.warn(error);
@@ -19,23 +21,18 @@ const Main = () => {
   }, []);
 
   const deleteAllTasks = async () => {
-    axios
-      .delete(`${url}/deleteAll`)
-      .then((result) => console.log(result.statusText))
-      .finally(() => window.location.reload());
+    axios.delete(`${url}/deleteAll`)
+    .then((result) => setTask(result.data.data));
   };
   const createTask = () => {
     if (text) {
       axios
         .post(`${url}/createTask`, { text: text })
         .then(function (response) {
-          console.log(response.statusText);
+          setTask(response.data.data);
         })
         .catch(function (error) {
           console.log(error);
-        })
-        .finally(() => {
-          window.location.reload();
         });
     }
   };
@@ -57,11 +54,15 @@ const Main = () => {
             setText(e.target.value);
           }}
         />
-        <button id="but1" onClick={() => createTask()}>
+        <button id="but1" 
+        title="Создать новую задачу"
+        onClick={() => createTask()}>
           Add
         </button>
-        <div className="control" id="leftControl">
-          <svg title="Удалить все задачи" id="deleteAll" onClick={deleteAllTasks} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <div className="control" 
+        id="leftControl" 
+        title="Удалить все задачи" >
+          <svg id="deleteAll" onClick={deleteAllTasks} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
             <path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z" />
           </svg>
         </div>
@@ -69,7 +70,10 @@ const Main = () => {
       <h2>Output will be here</h2>
       <div id="output">
         {allTasks.map((elem, index) => {
-          return <Card key={index} text={elem.text} checkbox={elem.isCheck} id={elem._id} />;
+          return <Card key={index} 
+          text={elem.text} 
+          checkbox={elem.isCheck} 
+          id={elem._id} />;
         })}
       </div>
     </div>
